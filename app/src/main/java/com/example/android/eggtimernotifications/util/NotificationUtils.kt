072c56bf0ee6c205939431f91c65946811808bd9
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-04-25 17:00:39 +0200

    Adding a Basic Simple Notification !

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -49,11 +49,17 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // TODO: Step 2.2 add snooze action
 
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
-    // Build the notification
-
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.egg_notification_channel_id)
+    )
     // TODO: Step 1.8 use the new 'breakfast' notification channel
 
-    // TODO: Step 1.3 set title, text and icon to builder
+        // TODO: Step 1.3 set title, text and icon to builder
+        .setSmallIcon(R.drawable.cooked_egg)
+        .setContentTitle(applicationContext
+            .getString(R.string.notification_title))
+        .setContentText(messageBody)
 
     // TODO: Step 1.13 set content intent
 
@@ -63,7 +69,9 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
         // TODO: Step 2.5 set priority
 
-    // TODO: Step 1.4 call notify
+    // TODO: Step 1.4 call notify to send the notification
+    // Deliver the notification
+    notify(NOTIFICATION_ID, builder.build())
 
 }
 
